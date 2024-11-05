be3ac7555363a75aa32b87c262b814497df887de
Iryna Bohomiakova <irinkavakula@gmail.com>
2024-11-05 23:53:44 +0100

    print out result

--- src/main/java/es/santander/ascender/proyecto03/App.java
+++ src/main/java/es/santander/ascender/proyecto03/App.java
@@ -7,7 +7,8 @@ package es.santander.ascender.proyecto03;
 public class App {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        long resultado = calculadora.sumar(4,5);
-        System.out.print(resultado);
+        long resultado = calculadora.sumar(47,5);
+        System.out.print("The result of calculation is - ");
+        System.out.println(resultado);
     }
 }
